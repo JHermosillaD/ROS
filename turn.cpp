0dba34abb2f2b3474275d5ad19f3a83b5a6cade6
Hermosilla <hermosilladiaz@gmail.com>
2020-04-06 15:16:27 -0500

    turn from quaternion odom

--- turn.cpp
+++ turn.cpp
@@ -14,22 +14,22 @@ double initPx, initPy, inityaw , inityaw_degrees;
 
 void turnCallback(const nav_msgs::Odometry::ConstPtr& odomMsg){
 	
-    // Quaternion to RPY conversion
-    tf::Quaternion q(
-        odomMsg->pose.pose.orientation.x,
-        odomMsg->pose.pose.orientation.y,
-        odomMsg->pose.pose.orientation.z,
-        odomMsg->pose.pose.orientation.w);
-    tf::Matrix3x3 m(q);
-    m.getRPY(roll, pitch, yaw);
+        // Quaternion to RPY conversion
+        tf::Quaternion q(
+            odomMsg->pose.pose.orientation.x,
+            odomMsg->pose.pose.orientation.y,
+            odomMsg->pose.pose.orientation.z,
+            odomMsg->pose.pose.orientation.w);
+        tf::Matrix3x3 m(q);
+        m.getRPY(roll, pitch, yaw);
     
 	// Initial conditions
-    if (flag == 0){
-        initPx =  odomMsg->pose.pose.position.x;
-        initPy =  odomMsg->pose.pose.position.y;
-        inityaw = yaw;
-        flag = 1;
-    }
+        if (flag == 0){
+        	initPx =  odomMsg->pose.pose.position.x;
+        	initPy =  odomMsg->pose.pose.position.y;
+        	inityaw = yaw;
+        	flag = 1;
+        }
     
     // Angular positions
 	yaw_degrees = yaw * 180.0 / M_PI; 
@@ -42,10 +42,10 @@ void turnCallback(const nav_msgs::Odometry::ConstPtr& odomMsg){
 		inityaw_degrees += 360.0; 						// convert negative to positive angles
 	}
 		
-	if( fabs(yaw_degrees - inityaw_degrees) <= 90.0) {  // turn 90 degrees
+	if( fabs(yaw_degrees - inityaw_degrees) <= 90.0) {  				// turn 90 degrees
 		moveMsg.linear.x = 0.0;
         moveMsg.angular.z = 0.2;
-    }
+        }
 	else {
 		moveMsg.linear.x = 0.0;
         moveMsg.angular.z = 0.0;
@@ -53,7 +53,7 @@ void turnCallback(const nav_msgs::Odometry::ConstPtr& odomMsg){
 
 	ROS_INFO("Initial angle -> %f, Current angle -> %f, Angular displacement -> %f\n", inityaw_degrees, yaw_degrees, fabs(yaw_degrees - inityaw_degrees) );
 
-    move_pub.publish(moveMsg);
+	move_pub.publish(moveMsg);
     
 }
 
