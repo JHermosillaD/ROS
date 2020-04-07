d41e99b6827727307965219637cb695bce770862
Hermosilla <hermosilladiaz@gmail.com>
2020-04-07 16:05:03 -0500

    ultrasonic by ROS

--- ultrasonic.ino
+++ ultrasonic.ino
@@ -1,6 +1,6 @@
-#include "ros.h"
-#include "std_msgs/Float64.h"
-#include "SR04.h"
+#include <ros.h>
+#include <std_msgs/Float64.h>
+#include <SR04.h>
 #define TRIG_PIN 12
 #define ECHO_PIN 11
 
@@ -15,7 +15,7 @@ void setup() {
 
 	n.initNode();
 	n.advertise(ultrasonic);
-	Serial.begin(9600);
+	Serial.begin(57600);
 	delay(1000);
 
 }
