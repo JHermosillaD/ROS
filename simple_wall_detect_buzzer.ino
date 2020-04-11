3b9b60602bed4a98ef735cafb3dd8e53a707b583
Hermosilla <hermosilladiaz@gmail.com>
2020-04-11 02:20:05 -0500

    Create simple_wall_detect_buzzer.ino

---
+++ simple_wall_detect_buzzer.ino
@@ -0,0 +1,82 @@
+#include <ros.h>
+#include <std_msgs/Float64.h>
+#include <SR04.h>
+#include <pitches.h>
+#define TRIG_PIN 12
+#define ECHO_PIN 11
+
+ros::NodeHandle n;
+std_msgs::String Control;
+std_msgs::Float64 Range;
+ros::Publisher Velocity("Velocity",&Control);
+ros::Publisher Ultrasonic("Ultrasonic",&Range);
+
+// SR04
+SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
+long a;
+
+//L293D
+// Driven wheels
+const int motorPinD1  = 5;  // Pin 14 of L293
+const int motorPinD2  = 6;  // Pin 10 of L293
+// Rotation wheels
+const int motorPinR1  = 10; // Pin  7 of L293
+const int motorPinR2  = 9;  // Pin  2 of L293
+
+// notes in the melody:
+int melody[] = { NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6 };
+int duration = 500;  // 500 miliseconds
+
+void setup(){
+
+    n.initNode();
+    n.advertise(Ultrasonic);
+    n.advertise(Velocity);
+    Serial.begin(57600);
+    
+    //Set pins as outputs
+    pinMode(motorPinD1, OUTPUT);
+    pinMode(motorPinD2, OUTPUT);
+    pinMode(motorPinR1, OUTPUT);
+    pinMode(motorPinR2, OUTPUT);
+
+    //Set speed control
+    int speedD = 80;            // 0-255  
+    int speedR = 50;            // 0-255
+    
+    delay(1000);   
+}
+void loop(){
+
+    // Ultrasonic range measure
+    a=sr04.Distance();
+    Range.data= a;
+    Ultrasonic.publish(&Range);
+    
+    // Motion Control
+
+    if( Range >= 10.0 || Range != Range ){
+      analogWrite(motorPinD1, speedD);
+      analogWrite(motorPinD2, speedD);
+      digitalWrite(motorPinR1, LOW);
+      digitalWrite(motorPinR2, LOW);
+      Control = " Straight line ";
+    }
+    else{
+      for (int thisNote = 0; thisNote < 8; thisNote++) {
+        // pin8 output the voice, every scale is 0.5 sencond
+        tone(8, melody[thisNote], duration);
+      }
+      digitalWrite(motorPinD1, LOW);
+      digitalWrite(motorPinD2, LOW);
+      analogWrite(motorPinR1, speedR);
+      analogWrite(motorPinR2, speedR);
+      Control = " Rotating ";
+    }
+
+    Velocity.publish(&Control);
+    ROS_INFO("Range -> %f, Status -> %s\n", Range, Control );
+
+    n.spinOnce();
+    delay(1000);
+}
